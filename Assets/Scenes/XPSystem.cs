8b7708dfcbcc2838aa5f3f8b5e22946b27075e8b
LiamDavie <1025803@student.sae.edu.au>
2022-02-18 13:07:34 +1100

    project work

--- Assets/Scenes/XPSystem.cs
+++ Assets/Scenes/XPSystem.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class XPSystem : MonoBehaviour
 {
-    public int Level; //Laps
-    public float curXP; //curLapProgress
-    public float reqXP; //reqLapProgress
+    public int Level = 1;      //Laps
+    public float curXP = 0;      //curLapProgress
+    public float reqXP = 100;     //reqLapProgress
 
     //stats variables (to be tracked and modified upon Level Up)
-    public float health;
-    public float defence;
-    public float speed;
-    public float attack;
+    public float health = 100;
+    public float defence = 50;
+    public float speed = 10;
+    public float attack = 10;
+    public float EnemyHealth = 30;
 
     // Start is called before the first frame update
     void Start()
@@ -23,20 +24,57 @@ public class XPSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
     public void initialstats()
     {
+        if (health > 0)
+        {
+            Level = level;
+            curXP = curXP;
+            defence = defence;
+            speed = speed;
+            attack = attack;
+        }
+        else
+        {
+            Level = 0;
+            curXP = 0;
+            defence = 0;
+            speed = 0;
+            attack = 0;
+            Debug.Log("You have fallen!");
+        }
         //initialize stats, level, xp, reqxp, etc
     }
 
     public void interaction() //look at button presses in RacingScript
     {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            EnemyHealth -= attack;
+            health -= 10;
+            Debug.Log("Attack for " + attack + ", health at " + health);
+
+        }
         //interaction caused by button press
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            health += 5;
+            Debug.Log("Defended, health at " + health);
+        }
+
+        if (EnemyHealth = 0)
+            //Enemy defeated
+            curXP + 20;
+        EnemyHealth = 30;
+
     }
 
     public void GainXP() //gain progress
     {
+        
+
         //increase our current XP
     }
 
