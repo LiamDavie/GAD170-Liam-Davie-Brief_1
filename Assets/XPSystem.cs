616328aa445fd183b4c992c9cc0b2e9d7a305615
LiamDavie <1025803@student.sae.edu.au>
2022-03-18 23:18:00 +1100

    final product

--- Assets/XPSystem.cs
+++ Assets/XPSystem.cs
@@ -4,42 +4,64 @@ using UnityEngine;
 
 public class XPSystem : MonoBehaviour
 {
-    public int Level = 1;      //Laps
-    public float curXP = 0;      //curLapProgress
-    public float reqXP = 100;     //reqLapProgress
+    public int level;      //Laps
+    public float curXP;      //curLapProgress
+    public float reqXP;     //reqLapProgress
 
     //stats variables (to be tracked and modified upon Level Up)
-    public float health = 100;
-    public float defence = 50;
-    public float speed = 10;
-    public float attack = 10;
-    public float EnemyHealth = 30;
+    public float health;
+    public float defence;
+    public float speed;
+    public float attack;
+    public float EnemyHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 100;
+        EnemyHealth = 30;
+        attack = 10;
+        defence = 20;
+        speed = 10;
+        Debug.Log("Welcome! Use left arrow key to attack and right arrow key to defend, to fight and level up. Current health is: " + health);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            EnemyHealth -= attack;
+            health -= 10;
+            Debug.Log("Attack for " + attack + ", health at " + health);
+            initialstats();
+
+        }
+        //interaction caused by button press
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            health += 5;
+            Debug.Log("Defended, health at " + health);
+            initialstats();
+        }
     }
     public void initialstats()
     {
         if (health > 0)
         {
-            Level = level;
+            level = level;
             curXP = curXP;
+            reqXP = 100;
             defence = defence;
             speed = speed;
             attack = attack;
+            interaction();
         }
         else
         {
-            Level = 0;
+            level = 0;
             curXP = 0;
+            reqXP = 100;
             defence = 0;
             speed = 0;
             attack = 0;
@@ -50,41 +72,48 @@ public class XPSystem : MonoBehaviour
 
     public void interaction() //look at button presses in RacingScript
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            EnemyHealth -= attack;
-            health -= 10;
-            Debug.Log("Attack for " + attack + ", health at " + health);
+        
+        Debug.Log("Enemy health is now: " + EnemyHealth);
+        if (EnemyHealth <= 0)
+            GainXP();
+      
 
-        }
-        //interaction caused by button press
-        if(Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            health += 5;
-            Debug.Log("Defended, health at " + health);
-        }
-
-        if (EnemyHealth = 0)
-            //Enemy defeated
-            curXP + 20;
-        EnemyHealth = 30;
 
     }
 
     public void GainXP() //gain progress
     {
-        
-
+            //Enemy defeated
+            curXP += 20;
+        EnemyHealth = 30;
+        Debug.Log("Enemy Defeated! Respawning enemy.");
         //increase our current XP
+        LevelUP();
     }
 
     public void LevelUP() //gain lap
     {
         //increase our level
+        if (curXP >= reqXP)
+        {
+            level++;
+            curXP = 0;
+            Debug.Log("Level Up! You are now Level " + level);
+            IncreaseStats();
+        }
+        if (curXP < reqXP)
+        {
+            Debug.Log("Current XP:" + curXP);
+        }
     }
 
     public void IncreaseStats()
     {
+        attack ++;
+        defence ++;
+        speed ++;
         //Improve our stats
+        Debug.Log("Stats increased! Attack now: " + attack + "! Defence now: " + defence + "! Speed now" + speed + "!");
     }
+
 }
